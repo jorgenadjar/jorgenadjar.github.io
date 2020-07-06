cbf11ebf96e2a2708b3cc2da4956644789a23f86
jorgenadjar <53016661+jorgenadjar@users.noreply.github.com>
2020-07-06 13:28:51 -0500

    Added isMobile to JS
    Added isMobile flag to disable parallax if on mobile

--- js/parallax.js
+++ js/parallax.js
@@ -3,13 +3,19 @@ $(window).scroll(function() {
   })
   
   function parallax() {
+
+    var ismobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+    if(!ismobile) {
   
-    var wScroll = $(window).scrollTop()
-  
-  
-    $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
-  
-  
-    $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
+      var wScroll = $(window).scrollTop()
+    
+    
+      $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+    
+    
+      $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
+
+    }
   
   }
